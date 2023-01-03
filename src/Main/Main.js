bedbe338a937c5f9bd276010095355d7293271ba
Ji Min Cha <jimin8830@naver.com>
2023-01-04 00:39:33 +0900

    firebase distribute

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Main.css";
 import Container from "../Component/Container";
 
 const Main = () => {
+  const [Number, SetNumber] = useState(0);
+
+  const SetPartNumber = (e) => {
+    SetNumber(e.target.value);
+    console.log(e.target.value);
+  };
   return (
     <Container>
       <h1>MUSINSA</h1>
 
       <div className="input_frame">
-        <h4>재고 확인</h4>
-        <input type="text" />
-        <button>입력</button>
+        <h4>상품 확인</h4>
+        <input type="text" placeholder="품번" onChange={SetPartNumber} />
+        <button>검색</button>
       </div>
     </Container>
   );
