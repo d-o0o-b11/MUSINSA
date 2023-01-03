9fb9ae6312e4bde01b62a71235f39bc38dd6cb28
Ji Min Cha <jimin8830@naver.com>
2023-01-04 01:59:17 +0900

    axios

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,22 +1,50 @@
 import React, { useState } from "react";
 import "./Main.css";
 import Container from "../Component/Container";
+import ListItem from "../Component/ListItem";
+import axios from "axios";
 
 const Main = () => {
   const [Number, SetNumber] = useState(0);
+  const [Size, SetSize] = useState(0);
 
   const SetPartNumber = (e) => {
     SetNumber(e.target.value);
-    console.log(e.target.value);
   };
+
+  const SetPartSize = (e) => {
+    SetSize(e.target.value);
+  };
+
+  const SendData = () => {
+    axios({
+      method: "post",
+      url: "http://52.79.196.124:8080/api/v1/product",
+      data: {
+        id: Number,
+        size: Size,
+      },
+    }).then((Response) => {
+      console.log(Response);
+    });
+  };
+
   return (
     <Container>
       <h1>MUSINSA</h1>
 
       <div className="input_frame">
-        <h4>상품 확인</h4>
+        <h4>재고 확인</h4>
         <input type="text" placeholder="품번" onChange={SetPartNumber} />
-        <button>검색</button>
+        <input type="text" placeholder="사이즈" onChange={SetPartSize} />
+        <button onClick={SendData}>검색</button>
+      </div>
+
+      <div className="list">
+        <ListItem>ddd</ListItem>
+        <ListItem>ddd</ListItem>
+        <ListItem>ddd</ListItem>
+        <ListItem>ddd</ListItem>
       </div>
     </Container>
   );
